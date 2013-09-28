022232e5cdc8e04de6df564e67ba68048714860a
Erik Nielsen <nielse63@gmail.com>
2013-09-28 12:33:38 -0500

    Calling up the .png

--- test/index.php
+++ test/index.php
@@ -10,7 +10,7 @@
 		// I've defined the base URL to be included in the source, as well as the images directory
 		$image = new ImageCache('http://test/test/images', dirname(__FILE__) . '/images');
 		// Include the filename without the path information
-		$d = $image->compress('chicago-skyline.jpg');
+		$d = $image->compress('chicago-skyline.png');
 	?>
 		<img src="<?php echo $d['src']; ?>" height="<?php echo $d['height']; ?>" width="<?php echo $d['width']; ?>">
 	
