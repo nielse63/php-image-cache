cbb2d4a1dc2f5f0b6eced5e2b84f0672f7b3d2e5
Erik Nielsen <nielse63@gmail.com>
2014-01-28 08:11:48 -0600

    Latest development changes.

--- demo/index.php
+++ demo/index.php
@@ -3,6 +3,13 @@
 date_default_timezone_set('UTC');
 require '../src/ImageCache/ImageCache.php';
 
+function debug( $a )
+{
+	echo '<pre>';
+	print_r( $a );
+	echo '</pre><hr>';
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -13,12 +20,17 @@ require '../src/ImageCache/ImageCache.php';
 <body>
 	<?php 
 		// I've defined the base URL to be included in the source, as well as the images directory
-		$image = new ImageCache\ImageCache( dirname(__FILE__) . '/images' );
+		$image = new ImageCache\ImageCache( dirname( __FILE__ ) . '/images', true, array( 'directory_name' => 'cached' ) );
 
 		// Compress the image with either the full URL source, or its location on the server
-		$d = $image->compress( 'http://placehold.it/500x300.gif');
+		$image->compress( 'http://placehold.it/500x300.gif' );
+		$image->compress( dirname( __FILE__ ) . '/images/500x300.gif' );
+		debug( $image );
+		/*
+			<img src="<?php echo $image->src; ?>" height="<?php echo $d['height']; ?>" width="<?php echo $d['width']; ?>">
+			<img src="<?php echo $image->src; ?>">
+		*/
 	?>
-		<img src="<?php echo $d['src']; ?>" height="<?php echo $d['height']; ?>" width="<?php echo $d['width']; ?>">
 	
 </body>
 </html>
--- src/ImageCache/ImageCache.php
+++ src/ImageCache/ImageCache.php
@@ -17,7 +17,6 @@
  *  - Continue to optimize for memory usage
  *  - Create a method that returns new images sizes
  *  - Create a method that resizes the image
- *
  */
 
 namespace ImageCache;
@@ -25,120 +24,148 @@ namespace ImageCache;
 class ImageCache
 {
 
-    /**
-     * @var string The current directory the class is being employed
-     */
-    private $root;
+	/**
+	 * @var string The current directory the class is being employed
+	 */
+	public $cached_directory;
 
-    /**
-     * @var string The root of the source image
-     */
-    private $src_root;
-    
-    /**
-     * @var bool If the directory containing the compressed images has already been created
-     */
-    private $created_dir;
-    
-    /**
-     * @var array Options set by the user for the class
-     */
-    private $opts;
-    
-    /**
-     * @var string The current directory the class is being employed
-     * 
-     * @todo Remove this variable or find a way that it can be removed
-     */
-    private $base;
-    
-    /**
-     * @var string|int The original memory limit set by the server
-     */
-    private $pre_memory_limit;
+	/**
+	 * @var string The root of the source image
+	 */
+	public $original_root;
 
-    /**
-     * The primary constructor function.  It sets up the environment and returns the class object
-     * 
-     * @param string|null $dir 
-     * @param bool $create_dir 
-     * @param array $opts 
-     * 
-     * @return self Returns the class object for contiuance
-     */
-    public function __construct( $dir = null, $create_dir = true, $opts = array() )
-    {
-        $defaults = array(
-            'quality' => 90,
-            'directory' => 'compressed'
-        );
+	/**
+	 * @var bool If the directory containing the compressed images has already been created
+	 */
+	private $created_dir;
 
-        if ( is_null( $dir ) ) {
-            $dir = dirname( dirname( __DIR__ ) );
-        }
+	/**
+	 * @var array Options set by the user for the class
+	 */
+	private $options;
 
-        $this->root = $dir;
-        $this->opts = array_merge( $defaults, $opts );
+	/**
+	 * @var string|int The original memory limit set by the server
+	 */
+	private $pre_memory_limit;
 
-        if ( ! $create_dir )
-            return $this;
-        return $this->createDirectory();
-    }
+	/**
+	 * @todo Add description
+	 */
+	static $compressed;
 
-    /**
-     * Creates the directory in which the cached images will be stored
-     * 
-     * @return self Returns the class object for contiuance
-     */
-    public function createDirectory()
-    {
-        if ( ! is_dir( $this->root . '/' . $this->opts['directory'] ) ) {
-            try {
-            	$cdir = $this->root . '/' . $this->opts['directory'];
-		        mkdir($cdir, 0777);
-		        $this->root = $cdir;
-		        $this->created_dir = true;
-		        return $this;
-            } catch (\Exception $e) {
-                echo 'There was an error creating the new directory:' . "\n";
-                $this->debug($e);
-            }
-        }
-        $this->src_root = $this->root;
-        $this->root .= '/' . $this->opts['directory'];
-        $this->created_dir = true;
-        return $this;
-    }
+	/**
+	 * @todo Add description
+	 */
+	private $original_image_source;
+
+	/**
+	 * @todo Add description
+	 */
+	public $src;
+
+	/**
+	 * The primary constructor function.  It sets up the environment and returns the class object
+	 * 
+	 * @param string $directory The base directory in which images are found - will soon be deprecated
+	 * @param bool $create_new_directory Whether or not to create a new directory - will soon be deprecated
+	 * @param object|array $opts The options array set by the user, or if none, defaults
+	 * 
+	 * @return self Returns the class object for contiuance
+	 */
+	public function __construct( $directory = null, $create_new_directory = true, $options = array() )
+	{
+		if ( is_null( $directory ) )
+			$directory = dirname( dirname( __DIR__ ) );
+
+		if( is_object( $options ) )
+			$options = (array) $options;
+
+		$defaults = array(
+			'search_directory' => $directory,
+			'quality' => 90,
+			'directory' => 'compressed', // For backwards compatibility, but will soon be deprecated
+			'directory_name' => 'compressed',
+			'create_cached_directory' => true
+		);
+
+		$this->original_root = $directory;
+		$this->options = array_merge( $defaults, $options );
+
+		if( isset( $defaults['create_cached_directory'] ) && $defaults['create_cached_directory'] )
+			return $this->createDirectory( $this->options['directory_name'] );
+
+		if ( ! $create_new_directory )
+			return $this;
+		return $this->createDirectory();
+	}
+
+	/**
+	 * Creates the directory in which the cached images will be stored
+	 * 
+	 * @return self Returns the class object for contiuance
+	 */
+	public function createDirectory( $directory_name = null )
+	{
+		if( ! $directory_name || is_null( $directory_name ) )
+			$directory_name = $this->options['directory_name'];
+
+		if( $this->created_dir && $this->cached_directory && is_dir( $this->cached_directory ) )
+			return $this;
+
+		$full_path = $this->original_root . '/' . $directory_name;
+		if ( ! is_dir( $full_path ) ) {
+			try {
+				mkdir( $full_path, 0755 );
+			} catch( \Exception $e ) {
+				return $this->throwError( 'There was an error creating the new directory:', $e );
+			}
+		}
+		$this->cached_directory = $full_path;
+		$this->created_dir = true;
+		return $this;
+	}
 
     /**
      * Reads the image and in turn compresses, relocations, and returns a cached copy
      * 
-     * @param string $src 
+     * @param string $src Either a local image, with the relative or absolute directory path set during instatiation, or a URL
      * 
      * @return array Information on the newly compressed image, including the new source with modtime query, the height, and the width
      */ 
-    public function compress( $src )
+    public function compress( $source )
     {
-        if( strpos( $src, 'http' ) !== false ) {
-	        if( ! $this->isLocal( $src) ) {
-	        	$info = pathinfo( $src );
-	        	$filename = $info['basename'];
-	        	$localfile = $this->src_root . '/' . $filename;
-	        	file_put_contents( $localfile, file_get_contents( $src ) );
-	        } else {
-	        	$localfile = $this->src_root . '/' . $filename;
-	        }
+		// Set variables for this method
+		$this->original_image_source = $source;
+		$filename = basename( $source );
+		$localfile = $this->cached_directory . '/' . $filename;
 
-	        if( ! file_exists( $localfile ) ) {
-	        	echo 'The image file could not be located';
-	        }
-        } else {
-	        if( ! file_exists( $src ) ) {
-	        	echo 'The image file could not be located';
-	        	exit;
-	        }
-	        $localfile = $src;
-        }
+		// Check if the image is already downloaded and cached
+		// if( $this->isInDirectory( $localfile ) )
+		// 	return $this->src = $this->makeSource( $localfile );
+
+		// If the image is derived from a filepath, return the URL
+		if( $this->isInDirectory( $source ) )
+			$source = $this->makeSource( $source );
+
+    	if( $this->isURL( $source ) && ! $this->is404( $source ) ) {
+    		$file_resource = file_get_contents( $source );
+    		try {
+    			file_put_contents( $localfile, $file_resource );
+    		} catch( \Exception $e ) {
+    			return $this->throwError( 'There was an error downloading the image:', $e );
+    		}
+    	} else {
+    		return $this->show404();
+    	}
+    	if( $this->isInDirectory( $localfile ) ) {
+	    	$this->src = $this->makeSource( $localfile );
+	    	$this->getMimeType( $localfile );
+	    	debug( $info );
+    	} else {
+    		return $this->throwError( 'There was an error downloading the image.  Try again.' );
+    	}
+    	/*
         $fileinfo = pathinfo( $localfile );
         $filename = $fileinfo['basename'];
         if( file_exists( $this->root . '/' . $filename ) ) {
@@ -180,12 +207,18 @@ class ImageCache
         $newinfo = getimagesize( $newlocation );
         $modtime = filemtime( $newlocation );
 	    $src = $this->makeSource( $this->root . '/' . $filename );
+        // $this->compressed = $src;
+
+        $this->src = $src;
+
+        // For backwards compatibility, but will soon be deprecated
         $out = array(
         	'src' => $src,
         	'width' => $newinfo[0],
         	'height' => $newinfo[1]
     	);
     	return $out;
+    	*/
     }
 
     /**
@@ -195,11 +228,48 @@ class ImageCache
      * 
      * @return string The name of the file sans extension
      */
-    public function getFilename( $file )
+    private function getFilename( $file )
     {
         $pathinfo = pathinfo( $file );
         $filename = $pathinfo['filename'];
         return $filename;
+    }
+
+    /**
+     * Description
+     * @param type $source 
+     * @return type
+     * 
+     * @todo Complete documentation
+     */
+    private function isURL( $source )
+    {
+    	if( filter_var( $source, FILTER_VALIDATE_URL ) )
+    		return true;
+    	return false;
+    }
+
+    private function isInDirectory( $source )
+    {
+    	if( is_file( $source ) && file_exists( $source ) )
+    		return true;
+    	return false;
+    }
+
+    private function is404( $source )
+    {
+    	$ch = curl_init();
+		curl_setopt( $ch, CURLOPT_URL, $main_url );
+		curl_setopt( $ch, CURLOPT_NOBODY, true );
+		curl_exec( $ch );
+		if( curl_getinfo( $ch, CURLINFO_HTTP_CODE ) == '404' )
+			return $this->show404();
+		return false;
+    }
+
+    private function show404()
+    {
+    	return $this->src = 'http://placehold.it/500x300&text=Image Not Found';
     }
 
     /**
@@ -227,7 +297,7 @@ class ImageCache
     private function makeSource( $dir )
     {
         $protocol = $_SERVER[ 'SERVER_PROTOCOL' ];
-        $protocol_array = explode( ':', $protocol );
+        $protocol_array = explode( '/', $protocol );
         $host = $_SERVER[ 'SERVER_NAME' ];
 
         $current_url_tmp = reset( $protocol_array ) . '://' . $host;
@@ -239,27 +309,26 @@ class ImageCache
         } else {
             $localpath = str_replace( $base, '', $dir );
         }
-        return $cururl . $localpath;
+        return $current_url . $localpath;
     }
 
-    /**
-     * Allocates memory usage to ensure that larger images can be handled without timing out
-     * 
-     * @param string $method The method being used to either "set" the new memory limit, or "reset" it back to the previous value
-     */
-    private function allocateMemory( $method )
+    private function increaseMemoryLimit()
     {
-        if ( $method === 'set' ) {
-            $amt = ini_get( 'memory_limit' );
-            $this->pre_memory_limit = $amt;
+    	if( isset( $this->pre_memory_limit ) )
+    		return;
+    	$memory_limit = ini_get( 'memory_limit' );
+    	$int_memory_limit = intval( $memory_limit );
+    	if( $int_memory_limit >= 128 )
+    		return;
+    	$this->pre_memory_limit = $int_memory_limit;
+    	ini_set( 'memory_limit', '128M' );
+    }
 
-            if ( intval( $amt ) < 128 ) {
-                ini_set( 'memory_limit', '128M' );
-            }
-        } else if ( $method === 'reset' ) {
-            $orig_mem = $this->pre_memory_limit;
-            ini_set( 'memory_limit', $orig_mem . 'M' );
-        }
+    private function resetMemoryLimit()
+    {
+    	if( ! isset( $this->pre_memory_limit ) )
+    		return;
+    	ini_set( 'memory_limit', $this->pre_memory_limit . 'M' );
     }
 
     /**
@@ -288,15 +357,43 @@ class ImageCache
         return false;
     }
 
+    private function getMimeType( $image )
+    {
+    	$info = getimagesize( $image );
+    	return $info['mime'];
+    }
+
     /**
-     * A basis debug function for printing output - good if not using unit testing
-     * 
-     * @param mixed $a The input variable
-     */
+    * A basic debug function for printing output - good if not using unit testing
+    * 
+    * @param mixed $a The input variable
+    */
     private function debug( $a )
     {
         echo '<pre>';
         print_r($a);
         echo '</pre><hr>';
+    }
+
+    /**
+     * Memory management function that releases an object's value
+     * 
+     * @param mixed $a Any input that is to be released
+     */
+    private function release( $a )
+    {
+        if( ! $a || is_null( $a ) )
+            return;
+        foreach( $a as $b ) {
+            unset( $b );
+        }
+        unset( $a );
+    }
+
+    private function throwError( $message = null, $error = null ) {
+    	if( is_null( $message ) )
+    		$message = 'Unknown Error:';
+		echo $message . "\n";
+		return $this->debug( $error );
     }
 }
