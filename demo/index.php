343ccd98f9d0d1f67a3eb7b0ee0261dc360aafa5
Matthew May <m@mrmsquared.com>
2022-03-30 15:02:32 -0400

    Update to support PHP 8.0+

--- demo/index.php
+++ demo/index.php
@@ -19,8 +19,12 @@
 	<?php
 		$imagecache = new ImageCache\ImageCache();
 		$imagecache->cached_image_directory = dirname(__FILE__) . '/images/cached';
-
+		// Test one with local file, also the same behavior as a file upload via PHP POST
 		$cached_src_one = $imagecache->cache( 'images/unsplash1.jpeg' );
+		// Show new cached filepath
+		echo '<p>Image Path: ' . $cached_src_one . '</p>';
+		// Show source filepath
+		echo '<p>Image Details: src - ' . $imagecache->image_src . ' filename - ' .  $imagecache->cached_filename . '</p>';
 		echo '<p>Original file size: ' . filesize($imagecache->image_src) . ' bytes</p>';
 		echo '<p>PHPImageCach-ified file size: ' . filesize($imagecache->cached_filename) . ' bytes</p>';
 		echo '<p>Total image size reduction: ' . (((filesize($imagecache->image_src) - filesize($imagecache->cached_filename)) / filesize($imagecache->image_src))*100) . '%</p>';
@@ -28,7 +32,7 @@
 	<img src="<?php echo $cached_src_one; ?>" alt="">
 	<hr>
 	<?php
-		$imagecache->check_link_cached = false;
+		// Test another local file
 		$cached_src_two = $imagecache->cache( 'images/unsplash2.jpeg' );
 		echo '<p>Original file size: ' . filesize($imagecache->image_src) . ' bytes</p>';
 		echo '<p>PHPImageCach-ified file size: ' . filesize($imagecache->cached_filename) . ' bytes</p>';
@@ -37,13 +41,13 @@
 	<img src="<?php echo $cached_src_two; ?>" alt="">
 	<hr>
 	<?php
-		$cached_src_two = $imagecache->cache( 'http://placehold.it/350x350' );
+		// Test downloading remote file
+		$cached_src_three = $imagecache->cache( 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg' );
 		echo 'Original file size: ' . filesize($imagecache->image_src) . ' bytes<br>';
 		echo 'PHPImageCach-ified file size: ' . filesize($imagecache->cached_filename) . ' bytes<br>';
-		echo 'Total image size reduction: ' . (((filesize($imagecache->image_src) - filesize($imagecache->cached_filename)) / filesize($imagecache->image_src))*100) . '%';
+		echo 'Total image size reduction: ' . (((filesize($imagecache->image_src) - filesize($imagecache->cached_filename)) / filesize($imagecache->image_src))*100) . '%</p>';
 	?>
-	<p>Because the "compressed" file size is larger than the original, a local copy of the original file will be server instead.</p>
-	<img src="<?php echo $cached_src_two; ?>" alt="">
+	<img src="<?php echo $cached_src_three; ?>" alt="">
 
 </body>
 </html>
